8f81f0cdff57ca59202746dcc1c9be3d6d69593b
Sebosun <sebos@protonmail.com>
2021-01-11 19:30:44 +0100

    moved part of the css from app.css to new navbar.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,18 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import "./Navbar.css"
 
 function Navbar(){
     return(
         <div className="navbar">
             <div className="navbarContainer">
-
                 <Link to="/">Seb's store</Link>
-
                 <div className="navbarList"> 
                     <ul>
                         <li>
                             <Link to="/store">Store</Link>
                         </li>
+
                         <li>
                             <Link to="/checkout">Checkout</Link>
                         </li>
