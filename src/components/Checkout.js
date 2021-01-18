c58d5fe0bb004eb582e357f69e69b68991a3e4a3
Sebosun <sebos@protonmail.com>
2021-01-18 15:56:39 +0100

    displaying total at the end of the checkout

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,14 +1,32 @@
-import React from "react"
+import {React, useState, useEffect} from "react"
 import "./Checkout.css";
 
 export default function Checkout(props){
-	let buttonDisplay = false;
+	const [buttonDisplay, setButtonDisplay] = useState(false);
+    const [total, setTotal] = useState(0);
+
+    useEffect(()=>{
+        let sum = 0;
+        console.log('USE EFFECT')
+        for (const index in props.items){
+            let count = props.items[index].count
+            let price = props.items[index].price
+            sum += count * price
+        }
+        setTotal(sum);
+    }, []);
+
+
     return(
-        <div class="checkoutContainer">
+        <div className="checkoutContainer">
         	{   
                 props.items.map((item, index)=>{
                 	{if (item.count > 0){
-                		buttonDisplay = true;
+                        // just placing it after item.count would cause an infinite loop of
+                        // rerendering, thus doing an if here
+                		if (buttonDisplay === false){
+                            setButtonDisplay(true);
+                        }
                 	
                 	return(
                         <div key={`${index}`} className="item">
@@ -17,6 +35,8 @@ export default function Checkout(props){
                         		<div style={{textTransform: 'capitalize'}}> {item.name} </div>
                         		<div>Quantity: {item.count}</div>
                         		<div>Price: ${item.price * item.count}</div>
+                                {/* {total += item.price * item.count} */}
+                                {/* {setButtonDisplay(true)} */}
                         	</div>
                         </div>
                         )
@@ -25,6 +45,7 @@ export default function Checkout(props){
                 })
 
             }
+            <div style={{display: buttonDisplay ? 'block':'none'}} className="total">Total: ${total}</div>
             <button style={{display: buttonDisplay ? 'block':'none'}}>Proceed to payment</button>
         </div>
     )
